e353716f3bc2924aef4a2dc1eedb5fe40e5a99d1
Elya <elinafromua@gmail.com>
2021-01-19 16:47:23 +0200

    initial commit

---
+++ src/main/java/com/progastination/BuckwheatSeeker.java
@@ -0,0 +1,11 @@
+package com.progastination;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuckwheatSeeker {
+    public static void main(String[] args) {
+        SpringApplication.run(BuckwheatSeeker.class, args);
+    }
+}
