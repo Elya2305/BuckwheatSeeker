b945bfd92a340d881eba1598c09ed0ca366d6d01
Elya <elinafromua@gmail.com>
2021-01-20 21:37:23 +0200

    make params not required

--- src/main/java/com/progastination/web/CategoryController.java
+++ src/main/java/com/progastination/web/CategoryController.java
@@ -10,8 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @Slf4j
 @RestController
 @AllArgsConstructor
@@ -20,16 +18,16 @@ public class CategoryController {
     private final CategoryService categoryService;
 
     @GetMapping("/main")
-    public PageDto<CategoryDto> mainCategories(@RequestParam int page,
-                                               @RequestParam int pageSize) {
+    public PageDto<CategoryDto> mainCategories(@RequestParam(required = false) int page,
+                                               @RequestParam(required = false) int pageSize) {
         log.info("Request on getting main categories");
         return categoryService.mainCategories(page, pageSize);
     }
 
     @GetMapping("/sub-categories")
     public PageDto<CategoryDto> subCategories(@RequestParam String identifier,
-                                           @RequestParam int page,
-                                           @RequestParam int pageSize) {
+                                           @RequestParam(required = false) int page,
+                                           @RequestParam(required = false) int pageSize) {
         log.info("Request on getting sub categories by identifier {}", identifier);
         return categoryService.subCategories(identifier, page, pageSize);
     }
